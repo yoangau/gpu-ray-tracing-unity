412853a9b1090d361d56bdf688f6c8f5b330bbed
Yoan Gauthier <yoangau@gmail.com>
2021-11-02 15:57:02 -0400

    complete basic setup

--- Assets/Scripts/RayTracingMaster.cs
+++ Assets/Scripts/RayTracingMaster.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RayTracingMaster : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public ComputeShader RayTracingShader;
+
+    private RenderTexture _target;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        
+        Render(destination);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Render(RenderTexture destination)
     {
-        
+        // Make sure we have a current render target
+        InitRenderTexture();
+
+        // Set the target and dispatch the compute shader
+        RayTracingShader.SetTexture(0, "Result", _target);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        // Blit the result texture to the screen
+        Graphics.Blit(_target, destination);
+    }
+
+    private void InitRenderTexture()
+    {
+        if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
+        {
+            // Release render texture if we already have one
+            if (_target != null)
+                _target.Release();
+
+            // Get a render target for Ray Tracing
+            _target = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _target.enableRandomWrite = true;
+            _target.Create();
+        }
     }
 }
